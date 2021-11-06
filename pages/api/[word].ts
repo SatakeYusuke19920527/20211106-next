7833723072cdc5fde92456fe0a7168c6db4369a9
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-11-06 21:50:01 +0900

    commit

--- pages/api/[word].ts
+++ pages/api/[word].ts
@@ -1,18 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiResponse } from 'next'
 import * as line from '@line/bot-sdk';
 
 const config = {
   channelAccessToken: process.env.NEXT_PUBLIC_LINE_ACCESS_TOKEN!,
+  channelSecret: process.env.NEXT_PUBLIC_LINE_CHANNEL_SECRET!
 };
 
 const client = new line.Client(config);
 
-export default ({ query: { word } }: { query: { word: string } }, res: NextApiResponse) => {
+export default async ({ query: { word } }: { query: { word: string } }, res: NextApiResponse) => {
   console.info('res data', word)
-  client.broadcast({
+  await client.broadcast({
     type: "text",
     text: word
-  }).then(data => console.log(data))
+  }).then(data => console.log("送信完了しました。",data))
     .catch(e => console.log(e))
   res.status(200).json({ message: `you requested for ${word} ` });
 };
