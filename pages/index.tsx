7ac6bc20f8421efececcb86936a1d8f512c66636
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-11-06 22:13:01 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,8 @@ console.log(process.env.NEXT_PUBLIC_API_DEV_URL!);
 const Home: NextPage = () => {
   const [text, setText] = useState<string>('');
   const sendLine = async () => {
+    console.log(`${URL}${text}`);
     try {
-      console.log(`${URL}${text}`);
       const response = await fetch(`${URL}${text}`);
       const data = await response.json();
       console.log('🚀 ~ file: index.tsx ~ line 11 ~ sendLine ~ data', data);
